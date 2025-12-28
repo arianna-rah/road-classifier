1632cff507970989fc9ea5566d6d34264d727791
Arianna Rahmathulla <106894369+arianna-rah@users.noreply.github.com>
2025-12-27 21:58:02 -0500

    Added balanced sampling and validation & validation dataset

--- classifier.py
+++ classifier.py
@@ -1,5 +1,11 @@
+import os
+import random
+from pathlib import Path
+from PIL import Image
+import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import torch
+import json
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
@@ -15,18 +21,27 @@ class_to_idx = {"dry": 0, "wet": 1, "standing_water": 2, "snow": 3, "ice": 4}
 class_names = ["dry", "wet", "standing_water", "snow", "ice"]
 
 dataset_path_train = "/kaggle/input/rscd-dataset-1million/RSCD dataset-1million/train"
-dataset_path_test = "/kaggle/input/rscd-dataset-1million/RSCD dataset-1million/train"
-
-
+dataset_path_test = "/kaggle/input/rscd-dataset-1million/RSCD dataset-1million/test_50k"
+dataset_path_vali = "/kaggle/input/rscd-dataset-1million/RSCD dataset-1million/vali_20k"
 
 class RSCDDataset(Dataset):
-    def __init__(self, root_dir, split="train", transform=None, label_map=None, class_names=None, class_to_idx=None):
-        self.root_dir = Path(root_dir) / split
+    def __init__(self, root_dir, transform, label_map, class_names, class_to_idx, max_samples, balanced=True, samples_file=None):
+        self.root_dir = Path(root_dir)
         self.transform = transform
         self.label_map = label_map
         self.class_names = class_names
         self.class_to_idx = class_to_idx
+        self.max_samples = max_samples
         self.samples = []
+
+        if samples_file is not None:
+            with open(samples_file, 'r') as infile:
+                self.samples = json.load(infile)
+            return
+
+        random.seed(42)
+
+        samples_by_class = {class_name: [] for class_name in self.class_names}
     
         for original_label in self.root_dir.iterdir():
             if original_label.isdir() and original_label.name in self.label_map:
@@ -34,8 +49,16 @@ class RSCDDataset(Dataset):
                 label_idx  = self.class_to_idx[mapped_label]
 
             for img_path in original_label.glob('*.jpg'):
-                self.samples.append((str(img_path), label_idx, mapped_label))
-    
+                samples_by_class[mapped_label].append((str(img_path), label_idx, mapped_label))
+        
+        if max_samples and balanced:
+            num_classes = len(self.class_names)
+            samples_by_class = max_samples // num_classes
+            for class_name, class_samples in samples_by_class.items():
+                n_to_take = min(samples_per_class, len(class_samples))
+                selected = random.sample(class_samples, n_to_take)
+                self.samples.extend(selected)
+
     def __len__(self):
         return self.samples
 
@@ -46,6 +69,57 @@ class RSCDDataset(Dataset):
             image = self.transform(image)
         
         return image, label_idx
+
+class RSCDDatasetTestVali(Dataset):
+    def __init__(self, root_dir, transform, label_map, class_names, class_to_idx, max_samples, balanced=True, samples_file=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.label_map = label_map
+        self.class_names = class_names
+        self.class_to_idx = class_to_idx
+        self.max_samples = max_samples
+        self.samples = []
+        self.samples_file = samples_file
+
+        if self.samples_file is not None:
+            with open(self.samples_file, 'r') as infile:
+                self.samples = json.load(infile)
+            return
+
+        random.seed(42)
+
+        samples_by_class = {class_name: [] for class_name in self.class_names}
+
+        for image_path in self.root_dir.iterdir():
+            if "dry" in str(image_path):
+                samples_by_class["dry"].append((str(image_path), self.class_to_idx["dry"], "dry"))
+            elif "wet" in str(image_path):
+                samples_by_class["wet"].append((str(image_path), self.class_to_idx["wet"], "wet"))
+            elif "water" in str(image_path):
+                samples_by_class["standing_water"].append((str(image_path), self.class_to_idx["standing_water"], "standing_water"))
+            elif "snow" in str(image_path):
+                samples_by_class["snow"].append((str(image_path), self.class_to_idx["snow"], "snow")]
+            elif "ice" in str(image_path):
+                samples_by_class["ice"].append((str(image_path), self.class_to_idx["ice"], "ice"))
+
+        if max_samples and balanced:
+            num_classes = len(self.class_names)
+            samples_by_class = max_samples // num_classes
+            for class_name, class_samples in samples_by_class.items():
+                n_to_take = min(samples_by_class, len(class_samples))
+                selected = random.sample(class_samples, n_to_take)
+                self.samples.extend(selected)
+                
+    def __len__(self):
+            return self.samples
+
+    def __getitem__(self, idx):
+        img_path, label_idx, label_name = self.samples[idx]
+        image = Image.open(img_path).convert('RGB') # don't know why .convert() is used. look into?
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label_idx 
             
 def find_distribution(dataset):
     labels = [sample[1] for sample in dataset.samples]
@@ -56,6 +130,53 @@ def find_distribution(dataset):
 
     return dict(zip(unique, counts))
 
-train_transform = 
+train_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.RandomCrop(size=(224, 224)),
+    transforms.RandomCrop(size=(224, 224)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])    
+])
 
-test_transform =
+test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+vali_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_dataset = RSCDDataset(dataset_path_train,  
+                            transform=train_transform, 
+                            label_map=label_map, 
+                            class_names=class_names,
+                            class_to_idx=class_to_idx,
+                            max_samples=160000)
+
+test_dataset = RSCDDatasetTestVali(dataset_path_test,
+                          transform=test_transform,
+                          label_map=label_map,
+                          class_names=class_names,
+                          class_to_idx=class_to_idx,
+                          max_samples=20000)
+vali_dataset = RSCDDatasetTestVali(dataset_path_vali,
+                          transform=test_transform,
+                          label_map=label_map,
+                          class_names=class_names,
+                          class_to_idx=class_to_idx,
+                          max_samples=20000)
+
+with open('train_samples.json', 'w') as fname:
+    json.dump(train_dataset.samples, f)
+
+with open('test_samples.json', 'w') as fname:
+    json.dump(test_dataset.samples, f)
+
+with open('vali_samples.json', 'w') as fname:
+    json.dump(vali_dataset.samples, f)
